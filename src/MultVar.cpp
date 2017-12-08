a0010875593a37df73c3129841db85743eff7a13
Daniel Trejo <daniel.trejobanos@unil.ch>
2017-12-08 13:22:39 +0100

    Included the original license message for MultVar.cpp

--- src/MultVar.cpp
+++ src/MultVar.cpp
@@ -1,3 +1,42 @@
+/**
+ * Multivariate Normal distribution sampling using C++11 and Eigen matrices.
+ *
+ * This is an extension of the multivariate normal simulation used in https://github.com/beniz/eigenmvn
+ *
+ * I included functionality necessary to sample from conditional gaussian random fields. Daniel Trejo Banos
+ *
+ * here is the original licence and message :
+ *
+ * This is taken from http://stackoverflow.com/questions/16361226/error-while-creating-object-from-templated-class
+
+ * (also see http://lost-found-wandering.blogspot.fr/2011/05/sampling-from-multivariate-normal-in-c.html)
+ *
+ * I have been unable to contact the original author, and I've performed
+ * the following modifications to the original code:
+ * - removal of the dependency to Boost, in favor of straight C++11;
+ * - ability to choose from Solver or Cholesky decomposition (supposedly faster);
+ * - fixed Cholesky by using LLT decomposition instead of LDLT that was not yielding
+ *   a correctly rotated variance
+ *   (see this http://stats.stackexchange.com/questions/48749/how-to-sample-from-a-multivariate-normal-given-the-pt-ldlt-p-decomposition-o )
+ */
+
+/**
+* Copyright (c) 2014 by Emmanuel Benazera beniz@droidnik.fr, All rights reserved.
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 3.0 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library.
+*/
+
 //TODO clean and test
 //verify the correct way to sample from conditional coef
 #include <Rcpp.h>
