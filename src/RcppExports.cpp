0ebdceb3bc87ed904f67dbdf5a244a4f397cb338
Daniel Trejo <daniel.trejobanos@unil.ch>
2018-02-12 13:44:28 +0100

    update RcppExports

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -7,8 +7,8 @@
 using namespace Rcpp;
 
 // BayesRSamplerV2
-void BayesRSamplerV2(std::string outputFile, int seed, int max_iterations, int burn_in, int thinning, Eigen::MatrixXd X, Eigen::VectorXd Y, double sigma0, double v0E, double s02E, double v0G, double s02G, double B);
-RcppExport SEXP _BayesRRcpp_BayesRSamplerV2(SEXP outputFileSEXP, SEXP seedSEXP, SEXP max_iterationsSEXP, SEXP burn_inSEXP, SEXP thinningSEXP, SEXP XSEXP, SEXP YSEXP, SEXP sigma0SEXP, SEXP v0ESEXP, SEXP s02ESEXP, SEXP v0GSEXP, SEXP s02GSEXP, SEXP BSEXP) {
+void BayesRSamplerV2(std::string outputFile, int seed, int max_iterations, int burn_in, int thinning, Eigen::MatrixXd X, Eigen::VectorXd Y, double sigma0, double v0E, double s02E, double v0G, double s02G, Eigen::VectorXd cva);
+RcppExport SEXP _BayesRRcpp_BayesRSamplerV2(SEXP outputFileSEXP, SEXP seedSEXP, SEXP max_iterationsSEXP, SEXP burn_inSEXP, SEXP thinningSEXP, SEXP XSEXP, SEXP YSEXP, SEXP sigma0SEXP, SEXP v0ESEXP, SEXP s02ESEXP, SEXP v0GSEXP, SEXP s02GSEXP, SEXP cvaSEXP) {
 BEGIN_RCPP
     Rcpp::RNGScope rcpp_rngScope_gen;
     Rcpp::traits::input_parameter< std::string >::type outputFile(outputFileSEXP);
@@ -23,8 +23,8 @@ BEGIN_RCPP
     Rcpp::traits::input_parameter< double >::type s02E(s02ESEXP);
     Rcpp::traits::input_parameter< double >::type v0G(v0GSEXP);
     Rcpp::traits::input_parameter< double >::type s02G(s02GSEXP);
-    Rcpp::traits::input_parameter< double >::type B(BSEXP);
-    BayesRSamplerV2(outputFile, seed, max_iterations, burn_in, thinning, X, Y, sigma0, v0E, s02E, v0G, s02G, B);
+    Rcpp::traits::input_parameter< Eigen::VectorXd >::type cva(cvaSEXP);
+    BayesRSamplerV2(outputFile, seed, max_iterations, burn_in, thinning, X, Y, sigma0, v0E, s02E, v0G, s02G, cva);
     return R_NilValue;
 END_RCPP
 }
